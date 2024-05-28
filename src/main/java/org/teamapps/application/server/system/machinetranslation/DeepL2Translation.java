d07551935b5783e0abd2f8e68ffc109e6ff5d30d
Matthias Bernstein <matthias.bernstein@teamapps.org>
2024-05-28 10:27:31 +0200

    translation with language order

--- src/main/java/org/teamapps/application/server/system/machinetranslation/DeepL2Translation.java
+++ src/main/java/org/teamapps/application/server/system/machinetranslation/DeepL2Translation.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -25,10 +25,80 @@ import java.util.*;
 
 public class DeepL2Translation {
 	public static final Set<String> SUPPORTED_LANGUAGES = new HashSet<>(Arrays.asList("ar", "bg", "cs", "da", "de", "el", "en", "es", "et", "fi", "fr", "hu", "id", "it", "ja", "ko", "lt", "lv", "no", "nl", "pl", "pt", "ro", "ru", "sk", "sl", "sv", "tr", "uk", "zh"));
+	public static final List<String> SUPPORTED_LANGUAGE_LIST = Arrays.asList(
+			"en",
+			"de",
+			"fr",
+			"es",
+			"it",
+			"nl",
+			"pt",
+			"pl",
+			"tr",
+			"bg",
+			"cs",
+			"da",
+			"el",
+			"et",
+			"fi",
+			"hu",
+			"id",
+			"lt",
+			"lv",
+			"no",
+			"ro",
+			"sk",
+			"sl",
+			"sv",
+			"ru",
+			"uk",
+			"ja",
+			"ko",
+			"ar",
+			"zh"
+	);
+
 	private final String apiKey;
 
 	public DeepL2Translation(String apiKey) {
 		this.apiKey = apiKey;
+	}
+
+	public static String getBestSourceLanguage(Collection<String> languageIsoSet) {
+		if (languageIsoSet.contains("de")) {
+			return "de";
+		} else if (languageIsoSet.contains("en")) {
+			return "en";
+		} else if (languageIsoSet.contains("fr")) {
+			return "fr";
+		} else if (languageIsoSet.contains("es")) {
+			return "es";
+		} else if (languageIsoSet.contains("nl")) {
+			return "nl";
+		} else if (languageIsoSet.contains("it")) {
+			return "it";
+		} else if (languageIsoSet.contains("ru")) {
+			return "ru";
+		}
+		return languageIsoSet.stream().toList().getFirst();
+	}
+
+	public static TextTranslationOptions createDefaultXmlEnabledOptions() {
+		TextTranslationOptions options = new TextTranslationOptions();
+		options.setSentenceSplittingMode(SentenceSplittingMode.NoNewlines);
+		options.setPreserveFormatting(true);
+		options.setTagHandling("xml");
+		options.setOutlineDetection(true);
+		options.setFormality(Formality.PreferLess);
+		return options;
+	}
+
+	public static TextTranslationOptions createDefaultPlainTextOptions() {
+		TextTranslationOptions options = new TextTranslationOptions();
+		options.setSentenceSplittingMode(SentenceSplittingMode.NoNewlines);
+		options.setPreserveFormatting(true);
+		options.setFormality(Formality.PreferLess);
+		return options;
 	}
 
 	public void printUsage() {
@@ -79,24 +149,6 @@ public class DeepL2Translation {
 			targetLang = "nb";
 		}
 		return targetLang;
-	}
-
-	public static TextTranslationOptions createDefaultXmlEnabledOptions() {
-		TextTranslationOptions options = new TextTranslationOptions();
-		options.setSentenceSplittingMode(SentenceSplittingMode.NoNewlines);
-		options.setPreserveFormatting(true);
-		options.setTagHandling("xml");
-		options.setOutlineDetection(true);
-		options.setFormality(Formality.PreferLess);
-		return options;
-	}
-
-	public static TextTranslationOptions createDefaultPlainTextOptions() {
-		TextTranslationOptions options = new TextTranslationOptions();
-		options.setSentenceSplittingMode(SentenceSplittingMode.NoNewlines);
-		options.setPreserveFormatting(true);
-		options.setFormality(Formality.PreferLess);
-		return options;
 	}
 
 }
